fc51e88c9be795ede761d3e0523332ce40fa66d7
BeemStream <beemstreamofficial@gmail.com>
2021-06-13 22:52:20 +0100

    chore: bump version

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-docker build -t beemstream/stream-collection-service .
-docker push beemstream/stream-collection-service
-ssh root@157.245.43.172 "docker service update --image beemstream/stream-collection-service beemstream_stream_collection_service"
 
+cargo bump patch
+
+version=$(awk -F'[ ="]+' '$1 == "version" { print $2 }' Cargo.toml)
+
+docker build -t beemstream/stream-collection-service:$version .
+docker push beemstream/stream-collection-service:$version
+
+# Tag image as latest
+docker pull beemstream/stream-collection-service:$version
+docker tag beemstream/stream-collection-service:$version beemstream/stream-collection-service:latest
+docker push beemstream/stream-collection-service:latest
+
+ssh root@157.245.43.172 "docker service update --image beemstream/stream-collection-service beemstream_stream_collection_service"
