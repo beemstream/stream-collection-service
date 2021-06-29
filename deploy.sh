1a3fbe7b77d3a17110e7097522c488df01e20e57
BeemStream <beemstreamofficial@gmail.com>
2021-06-29 17:46:53 +0100

    chore: update deploy script

--- deploy.sh
+++ deploy.sh
@@ -12,4 +12,4 @@ docker pull beemstream/stream-collection-service:$version
 docker tag beemstream/stream-collection-service:$version beemstream/stream-collection-service:latest
 docker push beemstream/stream-collection-service:latest
 
-ssh root@157.245.43.172 "docker service update --image beemstream/stream-collection-service beemstream_stream_collection_service"
+ssh root@157.245.43.172 "docker service update --image beemstream/stream-collection-service beemstream_stream_collection_service && docker container prune -f && docker image prune -a -f"
